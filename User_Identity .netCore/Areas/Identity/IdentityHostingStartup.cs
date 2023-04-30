0763b795fa26b5ba361ac0c187ce8a4926327134
alaay9003 <alaay9003@gmail.com>
2023-04-30 10:51:07 +0200

    Add project files.

---
+++ User_Identity .netCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using User_Identity_.netCore.Data;
+
+[assembly: HostingStartup(typeof(User_Identity_.netCore.Areas.Identity.IdentityHostingStartup))]
+namespace User_Identity_.netCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ User_Identity .netCore/Data/Migrations/20230427223308_asign-admin-all-role.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace User_Identity_.netCore.Data.Migrations
+{
+    public partial class asignadminallrole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [security].[UserRoles] (UserId,RoleId) SELECT 'f10c5ba4-cbf2-47bc-9da8-712031862be3',Id FROM [security].[Roles]");
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [security].[UserRoles] WHERE UserID = 'f10c5ba4-cbf2-47bc-9da8-712031862be3'");
+        }
+    }
+}
